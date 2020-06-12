5f0c69b2c777c6f30ba51a6ff06f55edff58644c
root <root@ip-172-31-33-46.us-east-2.compute.internal>
2020-06-12 22:54:56 +0000

    Light mode default map mode.

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@ function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
     center: {lat: 37.775, lng: -122.434},
-    mapTypeId: 'satellite'
+    mapTypeId: 'map'
   });
 
   heatmap = new google.maps.visualization.HeatmapLayer({
