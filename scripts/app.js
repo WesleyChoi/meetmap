e6d4a2fdcca5a6778b00735c3bc4c92e2a04c371
root <root@ip-172-31-33-46.us-east-2.compute.internal>
2020-06-12 22:58:30 +0000

    Fixed light/dark toggle.

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,11 @@
 var map, heatmap;
 
 function initMap() {
+  document.getElementById('darkmodebtn').style.display = 'block';
+  document.getElementById('lightmodebtn').style.display = 'none';
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
     center: {lat: 37.775, lng: -122.434},
-    mapTypeId: 'satellite'
   });
 
   heatmap = new google.maps.visualization.HeatmapLayer({
