6db01b77cc249a9f77c8663ad76796dffef0123d
k-chew <kailey.chew@uwaterloo.ca>
2020-06-13 12:58:14 -0700

    Created heatmap.js.

---
+++ scripts/heatmap.js
@@ -0,0 +1,29 @@
+function getHeatMap() {
+    var resultCentre = results[0].geometry.location;
+
+    // find bounds of grid
+    var resultLatSouth = lat(resultCentre) - 0.001; // around 100 m south
+    var resultLngWest = lng(resultCentre) - 0.001; // around 100 m west
+    // we're getting an around 200x200 square, width of each grid cell is:
+    var width = 0.001 / 10; 
+
+    // getting 441 points using a grid
+    var i, j; // rows, columns
+    var a = 0;
+    var newPoints;
+    for (i = 0; i < 21; i++) {
+        var lng = resultLngWest + (i * width);
+        for (j = 0; j < 21; j++) {
+            newPoints[a] = new google.maps.LatLng({
+                lat: resultLatSouth + (j * width),
+                lng: lng
+            });
+            ++a;
+        }
+    }
+
+    heatmap = new google.maps.visualization.HeatmapLayer({
+        data: newPoints,
+        map: map
+    });
+}
