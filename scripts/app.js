4d0ab956175138cd7e7f7461d87758973e15c634
Wesley <wesleyhchoi@gmail.com>
2020-06-12 17:58:57 -0700

    drag fix

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,6 @@ function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
     center: {lat: 37.775, lng: -122.434},
-    gestureHandling: 'none'
   });
   
 
